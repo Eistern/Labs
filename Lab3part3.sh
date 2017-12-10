fc4be312cbbe46e1db7ad6a0bfa99eaa7b4f7468
Zulin Daniil Konstantinovich <zulin@ccfit.nsu.ru>
2017-12-10 22:22:31 +0700

    Third Lab

---
+++ Lab3part3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Part3
+
+wget http://factorized.net/crusoe.txt
+egrep ' [zZ]' ./crusoe.txt
+egrep '[a-zA-Z]{16,16} ' ./crusoe.txt
+egrep -w '[aA][a-z]{1,}[c]' ./crusoe.txt
+egrep ' [aA][bB]' ./crusoe.txt | egrep -v ' [Aa][bB][oO]' 
+rm crusoe.txt
+
+wget http://factorized.net/patterns.txt
+egrep [0-9] ./patterns.txt | egrep -v [a-zA-Z]
+egrep [a-zA-Z] ./patterns.txt | egrep -v [0-9]
+egrep -w '[ABEKMHOPCTYX][0-9]{3,3}[ABEKMHOPCTYX]{2,2}[0-9]{2,3}' ./patterns.txt
