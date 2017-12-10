f0690fd7834688109dd853df67005110ffdec3c4
Zulin Daniil Konstantinovich <zulin@ccfit.nsu.ru>
2017-12-10 16:46:01 +0700

    Second Lab

--- Lab2.sh
+++ Lab2.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
+#Part 2
 whereis cat
-whereis
+whereis less
+whereis grep
+whereis find
+
+echo $PATH
+gcc hello.c
+./a.out
+cd ../
+Labs/a.out
+cd Labs
+export PATH=$PATH:pwd
+
+#Part 3
+cd ../
+touch shared.txt
+chmod go= u=r shared.txt
+chown bairamov shared.txt
+chown leonov /home/students/17200/leonov/shared.txt
+
+mkdir shared
+chmod go= u=r shared
+chown bairamov shared
+chown leonov /home/students/17200/leonov/shared
