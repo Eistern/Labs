ec2a8c016851d9655e53048fca198c04ed87cdbf
Zulin Daniil Konstantinovich <zulin@ccfit.nsu.ru>
2017-12-10 22:23:49 +0700

    Third Lab

--- Lab3part3.sh
+++ Lab3part3.sh
@@ -13,3 +13,6 @@ wget http://factorized.net/patterns.txt
 egrep [0-9] ./patterns.txt | egrep -v [a-zA-Z]
 egrep [a-zA-Z] ./patterns.txt | egrep -v [0-9]
 egrep -w '[ABEKMHOPCTYX][0-9]{3,3}[ABEKMHOPCTYX]{2,2}[0-9]{2,3}' ./patterns.txt
+rm patterns.txt
+
+exit 0
